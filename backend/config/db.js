b341c6999db1f88e5105bd4a6f5512098e16ae13
Alejandro <e.alvargas@go.ugr.es>
2025-01-23 17:02:18 +0100

    Add railway conf

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,19 @@
-const {Pool} = require('pg');
-const {db} = require('./config');
+const { Pool } = require('pg');
+const { db } = require('./config');
 
-const pool = new Pool({
-    user: db.user,
-    password: db.password,
-    host: db.host,
-    port: db.port,
-    database: db.database,
-});
+const pool = new Pool(
+  process.env.CI
+    ? { // Configuración para CI (GitHub Actions)
+        user: db.user,
+        password: db.password,
+        host: db.host,
+        port: db.port,
+        database: db.database
+      }
+    : { // Configuración para Producción (Railway)
+        connectionString: process.env.DATABASE_URL,
+        ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+      }
+);
 
 module.exports = pool;
